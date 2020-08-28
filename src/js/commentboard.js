f8a82855289af19c3075bdde232227563761dfa6
Prashant Verma <prashant.verma@timesinternet.in>
2020-08-28 23:55:09 +0530

    simple comment manipulation board is being added in the page.

---
+++ src/js/commentboard.js
@@ -0,0 +1,152 @@
+/**
+ * Comment component for the board
+ */
+var Comment = React.createClass({
+  /**
+   * Getting Initials state for the page
+   */
+  getInitialState: function () {
+    return { editMode: false };
+  },
+  /**
+   * saving the comment in the Array
+   * @param {*} event
+   */
+  saveComment: function (event) {
+    if (this.refs.commentText.value == "") {
+      alert("Please enter text to save");
+      return false;
+    }
+    this.props.saveComment(this.refs.commentText.value, this.props.id);
+    this.setState({ editMode: false });
+  },
+  /**
+   * Render the Form for taking the input of the User
+   */
+  renderForm: function () {
+    return (
+      <div className="comment-box" style={{ alignItems: "center" }}>
+        <textarea
+          style={{ width: "80%" }}
+          ref="commentText"
+          defaultValue={this.props.children}
+        ></textarea>
+        <button className="button-save" onClick={this.saveComment}>
+          save
+        </button>
+      </div>
+    );
+  },
+  /**
+   * Editing the comment
+   * @param {*} event
+   */
+  editComment: function (event) {
+    this.setState({ editMode: true });
+  },
+  /**
+   * Deleting a comment using the id props passed
+   * @param {*} event
+   */
+  deleteComment: function (event) {
+    this.props.deleteComment(this.props.id);
+  },
+  /**
+   * generating comments from the comment array
+   */
+  renderComment: function () {
+    return (
+      <div className="comment-box">
+        <span className="comment-text">{this.props.children}</span>
+        <button className="button-primary" onClick={this.editComment}>
+          edit
+        </button>
+        <button className="button-delete" onClick={this.deleteComment}>
+          delete
+        </button>
+      </div>
+    );
+  },
+  /**
+   * comment rendering method
+   */
+  render: function () {
+    return this.state.editMode ? this.renderForm() : this.renderComment();
+  },
+});
+/**
+ * Board component for the comments management.
+ */
+var Board = React.createClass({
+  /**
+   * Initially there are no comments on the board
+   */
+  getInitialState: function () {
+    return { comments: [] };
+  },
+  /**
+   * Adding comment on the board
+   */
+  addComment: function () {
+    let comments = this.state.comments;
+    // console.log(`adding comment...`);
+    comments.push(`Comment Default text..`);
+    this.setState({ comments: comments });
+  },
+  /**
+   * Deleting the comment from the board
+   * making this available to children via props.
+   * @param {*} index
+   */
+  deleteComment: function (index) {
+    let comments = this.state.comments;
+    comments.splice(index, 1);
+    this.setState({ comments: comments });
+  },
+  /**
+   * editing comment at a particular index
+   * @param {*} text
+   * @param {*} index
+   */
+  saveComment: function (text, index) {
+    let comments = this.state.comments;
+    comments[index] = text;
+    this.setState({ comments: comments });
+  },
+  /**
+   * Rendering comment on the board
+   */
+  renderComments: function () {
+    // console.log(this.state.comments);
+    return this.state.comments.map((text, index) => {
+      return (
+        <Comment
+          key={index}
+          id={index}
+          deleteComment={this.deleteComment}
+          saveComment={this.saveComment}
+        >
+          ({index + 1}) {text}
+        </Comment>
+      );
+    });
+  },
+  /**
+   * rendering the board
+   */
+  render: function () {
+    return (
+      <div>
+        <button className="button-comment-add" onClick={this.addComment}>
+          Add Comment
+        </button>
+        {this.renderComments()}
+      </div>
+    );
+  },
+});
+/**
+ * Showing multiple components created in the main parent.
+ * Showing multiple components in the Dom using ReactDom.
+ */
+ReactDOM.render(<Board />, document.getElementById("example"));
